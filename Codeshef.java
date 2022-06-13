f76899abd6ef6ba3f2f30fdb7cc07022c80396ba
Shrikrishna umbare <81535914+shri142@users.noreply.github.com>
2022-06-13 20:30:47 +0530

    Create Codeshef.java

---
+++ Codeshef.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc =new Scanner(System.in);
+		int a = sc.nextInt();
+		
+		for (int i=0;i<a ; i++) {
+		    
+		    int X =sc.nextInt();
+		    int Y =sc.nextInt();
+		    int A =sc.nextInt();
+		    
+		    if(A>=X && A<Y){
+		        
+		       System.out.print("YES\n");
+		    }
+		    else{
+		         System.out.print("NO\n");
+		    }
+		    
+		}
+	}
+}
