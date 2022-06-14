30f7bbe274fa60f754211ea394740a64648d8673
Shrikrishna umbare <81535914+shri142@users.noreply.github.com>
2022-06-14 14:45:59 +0530

    Create FirstContest.java

---
+++ FirstContest.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class FirstContest
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		
+		int N=sc.nextInt();
+		int A=sc.nextInt();
+		int B=sc.nextInt();
+		
+		
+		
+		System.out.println(N-A);
+		System.out.println(N-A-B);
+	}
+}
