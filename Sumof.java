fbe8d98bcd444d48c1198cb136552237e29e0421
Shrikrishna umbare <81535914+shri142@users.noreply.github.com>
2022-06-14 15:48:56 +0530

    Create Sumof.java

---
+++ Sumof.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class Sumof{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+       int n = sc.nextInt();
+        for(int i=0;i<n;i++){
+            int a = sc.nextInt();
+            int sumof = 0 ;
+            while(a>0){
+            int temp = a%10;
+            sumof +=temp;
+            a/=10;
+            }
+            System.out.println(sumof);
+        }
+    }
+}
