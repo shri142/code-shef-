907fcb7275b060498616cf741f7877fff96dcb3f
Shrikrishna umbare <81535914+shri142@users.noreply.github.com>
2022-06-14 20:49:03 +0530

    Create PackagingCupcakes.java

---
+++ PackagingCupcakes.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class PackagingCupcakes
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{  int T,a;
+	  Scanner sc= new Scanner(System.in);
+	  T= sc.nextInt();
+	  
+	  for (int i=0;i<T ;i++ ){
+	    a= sc.nextInt();
+	    
+	    System.out.println((a/2)+1);
+	      
+	  } 
+	}
+}
